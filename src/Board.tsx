ebb24a66105a4dc5934ea2c23e52b0b800143244
Gskoba <gskobelevff@gmail.com>
2019-09-04 02:26:19 +0300

    XO

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,6 +3,7 @@ import Square from './Square';
 
 interface IState {
     arr : Array<string>;
+    xIsNext : boolean;
 }
 
 interface Props {
@@ -13,7 +14,8 @@ export default class Board extends React.Component<Props, IState> {
     constructor(props : Props) {
         super(props);
         this.state = {
-            arr : Array(9).fill('')
+            arr : Array(9).fill(''),
+            xIsNext : true
         }
     }
 
@@ -26,13 +28,19 @@ export default class Board extends React.Component<Props, IState> {
 
     setXO(i : number) {
         const arr = this.state.arr.slice();
-        arr[i] = 'X';
-        this.setState({arr:arr});
+        const xIsNext = this.state.xIsNext;
+        arr[i] = xIsNext ? 'X' : 'O';
+        this.setState({
+            arr:arr,
+            xIsNext:!xIsNext
+        });
     }
 
     render() {
+        const status = 'Next move ' + (this.state.xIsNext ? 'X' : 'O'); 
         return (
             <>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.rendeItem(1)}
                     {this.rendeItem(2)}
